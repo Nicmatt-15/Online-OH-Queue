dae3310adc74c918229ed9f8fa3030ca997cb0a1
Nicolas <nicmatt@uw.edu>
2024-09-11 15:09:54 -0400

    updated UI and content to better align figjam

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-/* Login Related Elements */
+/* Login/Sign-up Related Elements */
+let authContainer = document.querySelector("#authContainer");
 let loginContainer = document.querySelector("#loginContainer");
+let signupContainer = document.querySelector("#signupContainer");
 let loginForm = document.querySelector("#loginForm");
 let modal = document.querySelector("#myModal");
 
@@ -11,53 +13,100 @@ let statusContainer = document.querySelector("#statusContainer");
 let profileButton = document.querySelector("#profileButton");
 let profileButtonText = document.querySelector("#profileButton svg text");
 
+/* This part of the code handles the toggling between
+login and signup form*/
+let loginTabButton = document.querySelector("#signinTab");
+let signupTabButton = document.querySelector("#signupTab");
+
+loginTabButton.addEventListener("click", () => {
+    loginContainer.classList.remove("hidden");
+    signupContainer.classList.add("hidden");
+});
+
+signupTabButton.addEventListener("click", () => {
+    loginContainer.classList.add("hidden");
+    signupContainer.classList.remove("hidden");
+});
+
+
 /* This part of the code handles the login submission form */
 loginForm.addEventListener("submit", () => {
     // Prevent the default form submission
     event.preventDefault();
 
     // Access form data
-    const username = document.querySelector("#username").value;
-    const password = document.querySelector("#password").value;
+    const loginEmail = document.querySelector("#loginEmail").value;
+    const loginPassword = document.querySelector("#loginPassword").value;
 
     // Log the form data to the console (or handle it as needed)
     console.log('Form submitted!');
-    console.log('Username:', username);
-    console.log('Password:', password);
+    console.log('Email:', loginEmail);
+    console.log('Password:', loginPassword);
 
-    closeLogin();
+    closeAuth();
     openStatus();
-    showProfileButton(username.substring(0, 1));
+    showProfileButton(loginEmail.substring(0, 1));
 })
 
 function openLogin() {
-    loginContainer.style.display = 'block';
+    authContainer.classList.remove("hidden");
 }
 
-function closeLogin() {
-    loginContainer.style.display = 'none';
+function closeAuth() {
+    authContainer.classList.add("hidden");
 }
 
 function showProfileButton(userFirstLetter) {
-    profileButton.removeAttribute("hidden");
+    profileButton.classList.remove("hidden");
     profileButtonText.textContent = userFirstLetter;
 }
 
-function openModal() {
-    modal.style.display = 'block';
-}
-
-function closeModal() {
-    modal.style.display = 'none';
-}
-
 function openStatus() {
-    statusContainer.style.display = 'block';
+    statusContainer.classList.remove("hidden");
 }
 
 function closeStatus() {
-    statusContainer.style.display = 'none';
+    statusContainer.classList.add("hidden");
 }
+
+/* This part of the code handles the checkbox revealing
+and unrevealing the password textbox */
+let loginPasswordInput = document.querySelector("#loginPassword");
+let signupPasswordInput = document.querySelector("#signupPassword");
+let loginPasswordCheckbox = document.querySelector("#loginShowPassword");
+let signupPasswordCheckbox = document.querySelector("#signupShowPassword");
+
+loginPasswordCheckbox.addEventListener("click", () => {
+    if (loginPasswordInput.type === "password") {
+        loginPasswordInput.type = "text";
+    } else {
+        loginPasswordInput.type = "password";
+    }
+})
+
+signupPasswordCheckbox.addEventListener("click", () => {
+    if (signupPasswordInput.type === "password") {
+        signupPasswordInput.type = "text";
+    } else {
+        signupPasswordInput.type = "password";
+    }
+})
+
+/* This part of the code handles opening and closing modal 
+when student clicks join office hour*/
+let joinOHButton = document.querySelector("#openModalButton")
+function openModal() {
+    modal.classList.remove("hidden");
+}
+
+function closeModal() {
+    modal.classList.add("hidden");
+}
+
+joinOHButton.addEventListener("click", () => {
+    openModal();
+});
+
 
 // Close the modal if the user clicks outside of it
 window.onclick = function(event) {
